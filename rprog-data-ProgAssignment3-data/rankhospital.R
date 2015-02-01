446406382beb56f9c4e02db81110a1febbd2c4af
neurofen <neurofen@gmail.com>
2015-02-01 20:25:38 +1100

    try different tack with direct dataframe manipulation and order function

--- rprog-data-ProgAssignment3-data/rankhospital.R
+++ rprog-data-ProgAssignment3-data/rankhospital.R
@@ -37,59 +37,28 @@ rankhospital <- function(state, outcome, num = "best") {
     
     
     ## create lookup matrix to match outcome to column number
-    getColForOutcome <- function() {
-        #validOutcomeLookup <- c(13, 19, 25)
-        validOutcomeLookup <- c(11, 17, 23)
-        #validOutcomeLookup <- c(17, 23, 29)
+    getColForOutcome <- function(validOutcomeLookup) {
         dim(validOutcomeLookup) <- c(1,3)
         colnames(validOutcomeLookup) <- validOutcomes 
         validOutcomeLookup[,outcome]
     }
     
-    # Prepare a dataset with rows matching state and outcome for numeric analysis
-    outcomeMeasuresForState <- subset(outcomeMeasures, outcomeMeasures[,7] == state)
+    ## 1. Get subset dataset for state
+    outcomeMeasuresForState <- subset(outcomeMeasures, outcomeMeasures[,7] == state)    
     
-    #print(outcomeMeasuresForState)
+    ## 2. Remove unwanted columns and rename column names
+    stateHospitalRates <- outcomeMeasuresForState[,c(2,11,17,23)]
+    names(stateHospitalRates) <- c("hospital", "heart attack", "heart failure", "pneumonia")
+ 
+    outcomeCol <-getColForOutcome(2:4)
+    ## 3. Convert all "Not Available" to NA  
+    stateHospitalRates[[outcomeCol]][stateHospitalRates[[outcomeCol]] == "Not Available"] <- NA
     
-    rslt <- outcomeMeasuresForState[,getColForOutcome()]
-    rslt[rslt == "Not Available"] <- NA
-    nresult <- as.numeric(rslt)
+    ## 4. Order ascending by outcome named column
+    ranked <- stateHospitalRates[order(as.numeric(stateHospitalRates[[outcomeCol]])),]
     
-    #print(nresult)
-    orderedRates <- sort( nresult[!is.na(nresult)])
-    print("Ordered Rates:")
-    print(orderedRates)
-    
-    #uniqueRates <- unique( sort( as.numeric(rslt[!is.na(rslt)])))   
-    #print("#### Mortality rates:")
-    #print(uniqueRates)
-    
-    if(!is.numeric(num)) {
-        if(identical("best", num)) {
-            num <- 1
-        }
-        else if(identical("worst", num)) {
-            num <- length(orderedRates)   
-        }
-        else {
-            num <- 0
-        }
-    }
-    
-    print("num is:")
-    print(num)
-    rankedRate <- orderedRates[orderedRates <= orderedRates[num]]    
-    print("#### Ranked rate:")
-    print(rankedRate)
-    
-    print("#### Result:")
-    
-    ## Return hospital name in that state with the given rank
-    ## 30-day death rate
+    ## 5. Only up to rank provided
+    ranked[,c(1, outcomeCol)][1:num,]
 
     
-    hospitalsMeetingRankedRate <- data.frame(subset( outcomeMeasuresForState, orderedRates <= orderedRates[num] )[,2], rankedRate, 1:length(rankedRate))
-    names(hospitalsMeetingRankedRate) <- c("hospital", "rate", "ran")
-    hospitalsMeetingRankedRate
-    #hospitalsMeetingRankedRate <- sort(subset( outcomeMeasuresForState, nresult <= orderedRates[num] )[,2])
 }
